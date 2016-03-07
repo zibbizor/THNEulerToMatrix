e642b5126079693f09089823d932d04b69aea561
AlleyLaptop <anthrax24@gmail.com>
2016-03-07 09:58:00 +0100

    Added stuff.

---
+++ THNEulerToMatrix/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace THNEulerToMatrix
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TextBoxX = new System.Windows.Forms.TextBox();
+            this.TextBoxZ = new System.Windows.Forms.TextBox();
+            this.TextBoxY = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TextBoxResult = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TextBoxX
+            // 
+            this.TextBoxX.Location = new System.Drawing.Point(13, 28);
+            this.TextBoxX.Name = "TextBoxX";
+            this.TextBoxX.Size = new System.Drawing.Size(100, 20);
+            this.TextBoxX.TabIndex = 0;
+            // 
+            // TextBoxZ
+            // 
+            this.TextBoxZ.Location = new System.Drawing.Point(225, 28);
+            this.TextBoxZ.Name = "TextBoxZ";
+            this.TextBoxZ.Size = new System.Drawing.Size(100, 20);
+            this.TextBoxZ.TabIndex = 1;
+            // 
+            // TextBoxY
+            // 
+            this.TextBoxY.Location = new System.Drawing.Point(119, 28);
+            this.TextBoxY.Name = "TextBoxY";
+            this.TextBoxY.Size = new System.Drawing.Size(100, 20);
+            this.TextBoxY.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(54, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "X";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(270, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Z";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(162, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Y";
+            // 
+            // TextBoxResult
+            // 
+            this.TextBoxResult.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TextBoxResult.Location = new System.Drawing.Point(3, 16);
+            this.TextBoxResult.Multiline = true;
+            this.TextBoxResult.Name = "TextBoxResult";
+            this.TextBoxResult.Size = new System.Drawing.Size(399, 55);
+            this.TextBoxResult.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(331, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.TextBoxResult);
+            this.groupBox1.Location = new System.Drawing.Point(3, 55);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(405, 74);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Result";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(413, 132);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TextBoxY);
+            this.Controls.Add(this.TextBoxZ);
+            this.Controls.Add(this.TextBoxX);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Euler to Matrix";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TextBoxX;
+        private System.Windows.Forms.TextBox TextBoxZ;
+        private System.Windows.Forms.TextBox TextBoxY;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TextBoxResult;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ THNEulerToMatrix/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SlimDX;
+using System.Globalization;
+
+namespace THNEulerToMatrix
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public float DegreeToRadian(float angle)
+        {
+            double r = Math.PI * angle / 180.0;
+            return (float)r;
+        }
+
+        public static Quaternion CreateFromYawPitchRoll(float yaw, float pitch, float roll)
+        {
+            float num = roll * 0.5f;
+            float num2 = (float)Math.Sin((double)num);
+            float num3 = (float)Math.Cos((double)num);
+            float num4 = pitch * 0.5f;
+            float num5 = (float)Math.Sin((double)num4);
+            float num6 = (float)Math.Cos((double)num4);
+            float num7 = yaw * 0.5f;
+            float num8 = (float)Math.Sin((double)num7);
+            float num9 = (float)Math.Cos((double)num7);
+            Quaternion result;
+            result.X = num9 * num5 * num3 + num8 * num6 * num2;
+            result.Y = num8 * num6 * num3 - num9 * num5 * num2;
+            result.Z = num9 * num6 * num2 - num8 * num5 * num3;
+            result.W = num9 * num6 * num3 + num8 * num5 * num2;
+            return result;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {            
+            float EulerX = float.Parse(TextBoxX.Text);
+            float EulerY = float.Parse(TextBoxY.Text);
+            float EulerZ = float.Parse(TextBoxZ.Text);
+
+            // X = pitch, Y = yaw, Z = roll,  
+            Quaternion q = CreateFromYawPitchRoll(DegreeToRadian(EulerY), DegreeToRadian(EulerX), DegreeToRadian(EulerZ));           
+            Matrix matrix = Matrix.RotationQuaternion(q);
+            
+            string THNOrientationX = "{ " + Math.Round(matrix.M11, 6).ToString(CultureInfo.InvariantCulture) + ", " + Math.Round(matrix.M12, 6).ToString(CultureInfo.InvariantCulture) + ", " + Math.Round(matrix.M13, 6).ToString(CultureInfo.InvariantCulture) + " }";
+            string THNOrientationY = "{ " + Math.Round(matrix.M21, 6).ToString(CultureInfo.InvariantCulture) + ", " + Math.Round(matrix.M22, 6).ToString(CultureInfo.InvariantCulture) + ", " + Math.Round(matrix.M23, 6).ToString(CultureInfo.InvariantCulture) + " }";
+            string THNOrientationZ = "{ " + Math.Round(matrix.M31, 6).ToString(CultureInfo.InvariantCulture) + ", " + Math.Round(matrix.M32, 6).ToString(CultureInfo.InvariantCulture) + ", " + Math.Round(matrix.M33, 6).ToString(CultureInfo.InvariantCulture) + " }";
+            string THNOrientation = "{" + THNOrientationX + " , " + THNOrientationY + " , " + THNOrientationZ + "}";
+
+            TextBoxResult.Text = THNOrientation;
+            //TextBoxResult.Text = EulerX.ToString() + EulerY.ToString() + EulerZ.ToString();
+
+        }
+    }
+}
